5c908dcd0cab5837778ba8f22574afb0e216b52f
Jakub Podgrodzki <podgrodzkijakub@gmail.com>
2025-02-21 16:50:06 +0100

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,37 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(Array.isArray(arrayReverse(['Mate', 'Academy']))).toBe(true);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return an empty array if only empty array was provided`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should reverse an array consisting of one word`, () => {
+    expect(arrayReverse(['Mate'])).toEqual(['etaM']);
+  });
+
+  it(`shoul reverse an array consisting of two words`, () => {
+    expect(arrayReverse(['Frock', 'coat'])).toEqual(['taock', 'corF']);
+  });
+
+  it(`shoul reverse an array consisting of many words`, () => {
+    expect(
+      arrayReverse(['In', 'hell', 'I', 'will', 'be', 'in', 'good', 'company']))
+      .toEqual(['yn', 'apmo', 'c', 'doog', 'ni', 'eb', 'lliw', 'IllehnI']);
+  });
+
+  it(`should preserve the length of every string`, () => {
+    const input = ['Mate', 'Academy'];
+    const reversed = arrayReverse(['Mate', 'Academy']);
+
+    expect(reversed.map(str => str.length))
+      .toEqual(input.map(str => str.length));
+  });
 });
